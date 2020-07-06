76ff70a0ee7ff86eb3a9a77863f956bcf18856fa
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-07-06 15:39:30 +0200

    Todo app

--- app/components/PageF/index.js
+++ app/components/PageF/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './PageF.scss';
+import TodoList from './TodoList.js';
 
 export default class PageF extends Component {
 	render() {
 		return (
 			<div>
-				Page F
+				<TodoList/>
 			</div>
 		);
 	}
