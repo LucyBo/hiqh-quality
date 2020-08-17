2c5d857fda6c3a4026b684281b5d1aa1234f57a6
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-08-17 20:40:57 +0200

    Updates

--- app/components/Footer copy/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import './Footer.scss';
-
-
-export default class HomePage extends Component {
-    render() {
-        return <div > Footer </div>;
-    }
-}
