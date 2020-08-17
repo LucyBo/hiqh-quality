e67832cd01bd0d1deca35472582e5dd02826f770
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-08-17 22:20:12 +0200

    clean up

--- app/components/HeaderMirror/index.js
+++ app/components/HeaderMirror/index.js
@@ -3,22 +3,22 @@ import './HeaderMirror.scss';
 import iphone from '../../images/iphone-mockup.png';
 import darenergii from '../../images/page.png';
 
-export default function HeaderMirror({title, id}) {
+export default function HeaderMirror({ title, id }) {
   return (
-    <section title="Section 5"
-    id="section5" className="HeaderMirror" >
+    <section title="Section 5" id="section5" className="HeaderMirror">
       <h1 className="text-center">My sample work</h1>
       <div className="row">
-      <div className="col-lg-6 col-sm-12 my-auto header-row">
-        <img src={darenergii} width="400" id="vid" />
-       
-      </div>
-      <div className="col-lg-6 col-sm-12 my-auto header-row">
-        <h4 className="text-center">Bioenergotherapist's page</h4>
-        <div className="right-col">
-        I used one from Gatsby starter, then completely makeover it. I prepared whole concept - design and functionality. Page has a little back-end app for sending data grabbed from contact form on mail. 
+        <div className="col-lg-6 col-sm-12 my-auto header-row">
+          <img src={darenergii} width="400" id="vid" />
         </div>
-      </div>
+        <div className="col-lg-6 col-sm-12 my-auto header-row">
+          <h4 className="text-center">Bioenergotherapist's page</h4>
+          <div className="right-col">
+            I used one from Gatsby starter, then completely makeover it. I
+            prepared whole concept - design and functionality. Page has a little
+            back-end app for sending data grabbed from contact form on mail.
+          </div>
+        </div>
       </div>
     </section>
   );
