6e74be0686928acf5e3cbb002d4999a78e65818c
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-08-17 22:16:58 +0200

    Updates

--- sw.js
+++ sw.js
@@ -109,7 +109,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2020-8-17 10:02:20 PM",
+  "version": "2020-8-17 10:16:55 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
